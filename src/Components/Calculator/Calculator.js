ea4f959dd84b39229d4a35cdf82921b1ecf4a33e
Crystal D'Souza <crystal.dsouza@ad.infosys.com>
2020-05-20 16:48:54 +0200

    .

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -11,46 +11,84 @@ class Calculator extends React.Component {
     super(props);
     this.state = {
       equation: "",
+      result: "",
     };
     this.updateEquation = this.updateEquation.bind(this);
     this.evaluate = this.evaluate.bind(this);
   }
 
   updateEquation(value) {
-    this.setState({ equation: this.state.equation.concat(value) });
+    let { equation, result } = this.state;
+
+    // if the entered value is an operator
+    // checking to see that you cannot add two operator together
+    if (
+      isNaN(value) &&
+      equation[equation.length - 1] &&
+      isNaN(parseFloat(equation[equation.length - 1]))
+    )
+      equation = equation.substring(0, equation.length - 1);
+
+    // if last value is a number and its 0, check if current value is a number, check if 2nd last value is an operator\
+    // eliminate teh zero
+    if (
+      !isNaN(equation[equation.length - 1]) &&
+      equation[equation.length - 1] === "0" &&
+      !isNaN(value) &&
+      (equation.length === 1 ||
+        operators.includes(equation[equation.length - 2]))
+    )
+      equation = equation.substring(0, equation.length - 1);
+
+    // handle adding of initial operators / *
+    if (equation.length === 0 && (value === "*" || value === "/")) {
+    } // do nothing
+    else equation = equation.concat(value);
+
+    // to avoid if last entered value was operator - just dont eval
+    if (!isNaN(value)) result = +eval(equation).toFixed(2); // the +helps to avoid .00 if no numbers after decimal
+
+    this.setState({ equation, result });
   }
 
   evaluate() {
     let { equation } = this.state;
     let { history } = this.props;
 
-    let result = eval(equation);
+    let result = +eval(equation).toFixed(2);
     history.push(equation + "=" + result);
     this.setState({
       equation: result.toString(),
       history: history,
+      result: "",
     });
     this.props.updateHistory(history);
   }
 
   cancel() {
-    let { equation } = this.state;
+    let { equation, result } = this.state;
     equation = equation.substring(0, equation.length - 1);
-    this.setState({ equation });
+    result = +eval(equation).toFixed(2);
+    this.setState({ equation, result });
   }
 
   clear() {
-    this.setState({ equation: "" });
+    this.setState({ equation: "", result: "" });
   }
 
   render() {
     const { history } = this.props;
-    const { equation } = this.state;
+    const { equation, result } = this.state;
     return (
       <div className={"calculator-container"}>
         <h2>{"My Calculator"}</h2>
         <div>
-          <div className={"display-screen"}>{equation}</div>
+          <div className={"display-screen"}>
+            <div>{equation}</div>
+            {!(equation === result.toString()) && (
+              <div className={"result-styling"}>{result}</div>
+            )}
+          </div>
 
           <div>
             <div className={"button-section-styling"}>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import mainReducer from "./reducer";
-import Calculator from "./Components/Calculator/Calculator";
 
 import App from "./Components/App";
 const store = createStore(mainReducer);
-// ReactDOM.render(<Calculator />, document.getElementById("root"));
 ReactDOM.render(
   <Provider store={store}>
     <App />
